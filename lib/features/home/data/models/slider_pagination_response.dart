a3d44b3b9974540a9f38cf2ca9da9e2d0bd79ef1
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-02-21 03:38:39 +0600

    added pagination slide data

---
+++ lib/features/home/data/models/slider_pagination_response.dart
@@ -0,0 +1,29 @@
+import 'package:ecommerce_ostad/features/home/data/models/slider_list_data_model.dart';
+
+class SlidePaginationResponse {
+  int? code;
+  String? status;
+  String? msg;
+  SliderListDataModel? data;
+
+  SlidePaginationResponse({this.code, this.status, this.msg, this.data});
+
+  SlidePaginationResponse.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    status = json['status'];
+    msg = json['msg'];
+    data = json['data'] != null ? SliderListDataModel.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['code'] = code;
+    data['status'] = status;
+    data['msg'] = msg;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    return data;
+  }
+}
+
