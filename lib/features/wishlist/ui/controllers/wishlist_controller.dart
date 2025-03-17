3d751247d7c88121386a69db8df7fb4535545139
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-03-17 23:12:54 +0600

    wishlist controller added

--- lib/features/wishlist/ui/controllers/wishlist_controller.dart
+++ lib/features/wishlist/ui/controllers/wishlist_controller.dart
@@ -1,12 +1,28 @@
 import 'package:ecommerce_ostad/app/urls.dart';
+import 'package:ecommerce_ostad/features/common/ui/controller/auth_controller.dart';
+import 'package:ecommerce_ostad/features/product/data/models/product_details_data_receiver_model.dart';
+import 'package:ecommerce_ostad/features/product/data/models/product_details_model.dart';
 import 'package:ecommerce_ostad/features/product/review/ui/models/review.dart';
-import 'package:ecommerce_ostad/features/product/review/ui/models/review_model.dart';
+import 'package:ecommerce_ostad/features/product/ui/controllers/product_details_controller.dart';
 import 'package:ecommerce_ostad/features/wishlist/ui/models/wishlist_model.dart';
 import 'package:ecommerce_ostad/services/network%20caller/network_caller.dart';
 import 'package:get/get.dart';
 
 class WishListController extends GetxController {
   bool _inProgress = false;
+
+  final ProductDetailsController _productDetailsController =
+      ProductDetailsController();
+
+  List<ProductDetailsDataReceiverModel?> _wishListProducts = [];
+
+  List<ProductDetailsModel> get wishListProductDetails {
+    return _wishListProducts
+        .map((receiver) => receiver?.data!)
+        .where((details) => details != null)
+        .cast<ProductDetailsModel>()
+        .toList();
+  }
 
   bool get inProgress => _inProgress;
 
@@ -16,6 +32,8 @@ class WishListController extends GetxController {
 
   String? _errorMessage;
 
+  AuthController authController = AuthController();
+
   String? get errorMessage => _errorMessage;
 
   Future<bool> getWishLists() async {
@@ -24,12 +42,22 @@ class WishListController extends GetxController {
     update();
 
     final NetworkResponse response =
-    await Get.find<NetworkCaller>().getRequest(Urls.wishListsUrl);
+        await Get.find<NetworkCaller>().getRequest(Urls.wishListsUrl);
     if (response.isSuccess) {
-      WishListModel wishListModel = WishListModel.fromJson(response.responseData);
-      List<String> productIds = wishListModel.data?.results?.map((item) {
-        return item.product?.sId ?? "";
-      }).where((id) => id.isNotEmpty).toList() ?? [];
+      WishListModel wishListModel =
+          WishListModel.fromJson(response.responseData);
+      List<String> productIds = wishListModel.data?.results
+              ?.map((item) {
+                return item.product?.sId ?? "";
+              })
+              .where((id) => id.isNotEmpty)
+              .toList() ??
+          [];
+
+      _wishListProducts = await Future.wait(productIds.map((productId) async {
+        await _productDetailsController.getProductDetails(productId);
+      }));
+
       isSuccess = true;
     } else {
       _errorMessage = response.errorMessage;
--- lib/features/wishlist/ui/screens/wish_list_screen.dart
+++ lib/features/wishlist/ui/screens/wish_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce_ostad/features/common/ui/controller/main_bottom_nav_controller.dart';
+import 'package:ecommerce_ostad/features/common/ui/widgets/centered_circular_progress_indicator.dart';
 import 'package:ecommerce_ostad/features/common/ui/widgets/product_item_widget.dart';
+import 'package:ecommerce_ostad/features/wishlist/ui/controllers/wishlist_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,6 +13,12 @@ class WishListScreen extends StatefulWidget {
 }
 
 class _WishListScreenState extends State<WishListScreen> {
+  @override
+  void initState() {
+    Get.find<WishListController>().getWishLists();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return PopScope(
@@ -26,18 +34,27 @@ class _WishListScreenState extends State<WishListScreen> {
             onPressed: _onPop,
           ),
         ),
-        body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            childAspectRatio: 0.8,
-            crossAxisSpacing: 4,
-            mainAxisSpacing: 3,
-          ),
-          itemCount: 20,
-          itemBuilder: (context, index) {
-            // return const FittedBox(child: ProductItemWIdget());
-          },
-        ),
+        body: GetBuilder<WishListController>(builder: (controller) {
+          if(controller.inProgress){
+            return const CenteredCircularProgressIndicator();
+          }
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 0.8,
+              crossAxisSpacing: 4,
+              mainAxisSpacing: 3,
+            ),
+            itemCount: controller.wishListProductDetails.length,
+            itemBuilder: (context, index) {
+              var product = controller.wishListProductDetails[index];
+              return FittedBox(
+                  child: ProductItemWIdget(
+                productModel: product,
+              ));
+            },
+          );
+        }),
       ),
     );
   }
